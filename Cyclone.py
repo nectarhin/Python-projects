0d87ab35a79a8a1224098e6d79586ceb4053b2d3
nectarhin <109590784+nectarhin@users.noreply.github.com>
2023-03-12 15:14:21 +0200

    Add files via upload

---
+++ Cyclone.py
@@ -0,0 +1,13 @@
+credit = 9
+height = 137
+withtallerperson = True
+if height >= 137 and credit >= 10:
+    print('Enjoy the ride!')
+if height < 200:
+    print('Enjoy the ride')
+elif height < 100 or withtallerperson != True:
+    print("You're not tall enough for this ride yet")
+elif height >= 100 and withtallerperson == True:
+    print('Enjoy the ride')
+else: credit < 10
+print('You dont have enough credits to ride')
